91740edc066c2565df1ebf7d1111d473eb47d391
OGMalin <oddg@sjakk.biz>
2015-04-21 23:38:12 +0200

    *

--- build.cmd
+++ build.cmd
@@ -1,21 +1,19 @@
 REM 7z.exe a commandline compression app from 7-zip
-move /y media media_org
 del PolarBook.zip
 mkdir admin
 mkdir site
-mkdir media
-xcopy /d /y /s media_org\com_polarbook\*.* media\*.*
+mkdir com_polarbook
 copy administrator\components\com_polarbook\polarbook.xml polarbook.xml
 xcopy /d /y /s administrator\components\com_polarbook\*.* admin\*.*
 xcopy /d /y /s components\com_polarbook\*.* site\*.*
+xcopy /d /y /s media\com_polarbook\*.* com_polarbook\*.*
 del admin\polarbook.xml
-..\7z a -Y PolarBook.zip media
-rmdir /S /Q media
 ..\7z a -Y PolarBook.zip *.xml
 ..\7z a -Y PolarBook.zip admin
 ..\7z a -Y PolarBook.zip site
+..\7z a -Y PolarBook.zip com_polarbook
 del polarbook.xml
 rmdir /S /Q admin
 rmdir /S /Q site
-move /Y media_org media
+rmdir /S /Q com_polarbook
 
